0bcd38ea58baafb55b8b007b706829f40bd8dcd8
Dedan Irungu <dedanirungu@gmail.com>
2022-09-14 12:19:07 +0300

    changes to menu

--- menu.php
+++ menu.php
@@ -4,12 +4,10 @@ $this->add_module_info("products", [
     'title' => 'Products',
     'description' => 'Products',
     'icon' => 'fas fa-store',
-    'path' => '/products',
+    'path' => '/product/admin/product',
     'class_str' => 'text-green-800 border-green-800'
 ]);
 
 
-$this->add_menu("products", "dashboard", "Dashboard", "/dashboard", "fas fa-cogs", 1);
-$this->add_menu("products", "products", "Products & Services", "/products/product-service", "fas fa-cogs", 5);
-$this->add_menu("products", "category", "Categories", "/products/product-categories", "fas fa-cogs", 5);
-$this->add_menu("products", "inventory", "Inventory", "/products/inventory", "fas fa-cogs", 5);
+$this->add_menu("products", "products", "Products", "/product/admin/product", "fas fa-cogs", 5);
+$this->add_menu("products", "category", "Categories", "/product/admin/category", "fas fa-cogs", 5);
