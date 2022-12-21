99afc90d0bcabd017b96c1df60ca3a478e823b3b
Dedan Irungu <dedanirungu@gmail.com>
2022-12-21 16:31:28 +0300

    Update

--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@ $this->add_module_info("products", [
     'class_str' => 'text-green-800 border-green-800'
 ]);
 
-
+ 
 $this->add_menu("products", "products", "Products", "/product/admin/product", "fas fa-cogs", 5);
 $this->add_menu("products", "category", "Categories", "/product/admin/category", "fas fa-cogs", 5);
 */
