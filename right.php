db0c0e8761b60755c8cb0abafbc60f42205d4ec6
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:14 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,8 @@
+<?php
+
+$this->add_right("product", "category", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("product", "category", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("product", "category", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("product", "category", "staff", view:true, add:true, edit:true);
+$this->add_right("product", "category", "registered", view:true, add:true);
+$this->add_right("product", "category", "guest", view:true, );
