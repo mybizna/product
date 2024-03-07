7fce670e2c96adc6a3013243a469d1e226cba87f
Dedan <dedanirungu@gmail.com>
2024-03-07 21:09:29 +0300

    Update Product entity with new fields and structure

--- Entities/Product.php
+++ Entities/Product.php
@@ -48,10 +48,18 @@ class Product extends BaseModel
 
         $this->fields->increments('id')->html('hidden');
         $this->fields->string('name')->nullable()->html('text');
-        $this->fields->integer('product_type_id')->nullable()->html('text');
-        $this->fields->integer('category_id')->nullable()->html('text');
-        $this->fields->integer('tax_cat_id')->nullable()->html('text');
-        $this->fields->integer('vendor')->nullable()->html('text');
+        $this->fields->integer('description')->nullable()->html('editor');
+        $this->fields->integer('category_id')->nullable()->html('recordselect');
+        $this->fields->integer('sub_category_id')->nullable()->html('recordselect');
+        $this->fields->integer('tags')->nullable()->html('tags');
+        $this->fields->integer('sku')->nullable()->html('text');
+        $this->fields->integer('discount')->nullable()->html('records');
+        $this->fields->integer('size')->nullable()->html('records');
+        $this->fields->integer('color')->nullable()->html('records');
+        $this->fields->integer('width')->nullable()->html('text');
+        $this->fields->integer('height')->nullable()->html('text');
+        $this->fields->integer('weight')->nullable()->html('text');
+        $this->fields->integer('shipping_cost', 20, 2)->default(0.00)->html('amount');
         $this->fields->decimal('cost_price', 20, 2)->default(0.00)->html('amount');
         $this->fields->decimal('sale_price', 20, 2)->default(0.00)->html('amount');
     }
@@ -62,12 +70,21 @@ class Product extends BaseModel
     public function structure($structure): array
     {
 
-        $structure['table'] = ['name', 'product_type_id', 'category_id', 'tax_cat_id', 'vendor', 'cost_price', 'sale_price'];
+        $structure['table'] = ['name', 'description', 'category_id', 'tax_cat_id', 'vendor', 'cost_price', 'sale_price'];
+
         $structure['form'] = [
             ['label' => 'Product Name', 'class' => 'col-span-full', 'fields' => ['name']],
             ['label' => 'Product Detail', 'class' => 'col-span-full md:col-span-6ull md:col-span-6ull  md:col-span-6 md:pr-2', 'fields' => ['product_type_id', 'category_id', 'tax_cat_id']],
             ['label' => 'Other Product Setting ', 'class' => 'col-span-full md:col-span-6ull  md:col-span-6 md:pr-2', 'fields' => ['vendor', 'cost_price', 'sale_price']],
+            ['label' => '', 'class' => 'col-span-full', 'tabs' => [
+                ['label' => 'Size', 'class' => 'col-span-full', 'fields' => ['size']],
+                ['label' => 'Color', 'class' => 'col-span-full', 'fields' => ['color']],
+            ]], 
+            ['label' => '', 'class' => 'col-span-full', 'tabs' => [
+                ['label' => 'Discount', 'class' => 'col-span-full', 'fields' => ['discount']],
+            ]],
         ];
+
         $structure['filter'] = ['name', 'product_type_id', 'category_id', 'tax_cat_id'];
 
         return $structure;
