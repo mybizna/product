3681567302b7dbb07363bfd193e89bc314efcd6c
Dedan <dedanirungu@gmail.com>
2024-04-12 11:12:29 +0300

    Add rights() method to Category, Product, Stock, and Type models

--- Entities/Product.php
+++ Entities/Product.php
@@ -97,6 +97,12 @@ class Product extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 }
